a78f8178dbb37d1f9609aa6a53d8d8144a054749
Dean Baberowski <deanbaberows@gmail.com>
2025-09-05 15:38:08 +1000

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import Article from './Article'
 import Header from './Header'
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
+
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div className="App">
+      learn react!
+      <Header />
+      <About />
+      <Article />
+    </div>
+  );
 }
 
 export default App;
